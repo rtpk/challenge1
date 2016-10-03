168be3072656aeb65bf5cfb5ed1157e9e9d50f17
rtpk <robert.piotrowski@gft.com>
2016-10-03 14:42:59 +0200

    first

---
+++ src/main/java/com/technical/cal/Calendar.java
@@ -0,0 +1,55 @@
+package com.technical.cal;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Iterator;
+
+/**
+ * Created by Robert Piotrowski on 03/10/2016.
+ */
+
+public class Calendar implements Iterable<LocalDate> {
+
+    private LocalDate startDate;
+
+    public Calendar(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    @Override
+    public Iterator<LocalDate> iterator() {
+        Iterator<LocalDate> it = new Iterator<LocalDate>() {
+
+            LocalDate state = startDate;
+
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public LocalDate next() {
+                if (state.getDayOfWeek().equals(DayOfWeek.THURSDAY))
+                    state = state.with(TemporalAdjusters.next(DayOfWeek.FRIDAY));
+                else
+                    state = state.with(TemporalAdjusters.next(DayOfWeek.THURSDAY));
+                return state;
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+        return it;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+}
---
+++ src/test/java/com/technical/CalendarTests.java
@@ -0,0 +1,7 @@
+package com.technical;
+/**
+ * Created by Robert Piotrowski on 03/10/2016.
+ */
+public class CalendarTests {
+
+}
