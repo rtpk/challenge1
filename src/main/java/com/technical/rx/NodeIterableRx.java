fc27108a51a92b7e9b6876bb6f093f5423a6a388
rtpk <robert.piotrowski@gft.com>
2016-11-08 11:54:19 +0100

    simple working rxjava

--- src/main/java/com/technical/rx/NodeIterableRx.java
+++ src/main/java/com/technical/rx/NodeIterableRx.java
@@ -1,11 +1,32 @@
 package com.technical.rx;
 
+import com.technical.node.NodeIterable;
+import lombok.extern.slf4j.Slf4j;
 import rx.Observable;
+import rx.observables.BlockingObservable;
 
-class NodeIterableRx<T> {
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
-   public Observable<T> convert(Iterable<T> source) {
+@Slf4j
+public class NodeIterableRx<T> {
 
-        return  null; //Observable<T>
+    public BlockingObservable<T> convert(NodeIterable<T> source) {
+        return Observable.interval(1, TimeUnit.SECONDS)
+                .concatMapIterable(x -> childrenOf(source))
+                .distinct()
+                .toBlocking();
+    }
+
+    private List<T> childrenOf(NodeIterable<T> root) {
+        return (List<T>) StreamSupport.stream(root.spliterator(), false).map(o -> (o)).collect(Collectors.toList());
+    }
+
+    private void print(File obj) {
+        log.info("Got: {}", obj.getAbsoluteFile());
     }
 }
+
--- src/test/java/com/technical/ReactiveTests.java
+++ src/test/java/com/technical/ReactiveTests.java
@@ -1,41 +1,70 @@
 package com.technical;
 
 import com.technical.file.NodeFile;
-import com.technical.node.Node;
 import com.technical.node.NodeIterable;
+import com.technical.rx.NodeIterableRx;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
-import rx.Observable;
+import rx.observables.BlockingObservable;
 
 import java.io.File;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class ReactiveTests {
 
+//    @Test
+//    public void testIteratorExists() {
+//        File file = new File("C:\\Users\\rtpk\\Downloads\\New folder");
+//        NodeIterable<File> root = new NodeIterable<>(new NodeFile(file));
+//        Iterator<File> it = root.iterator();
+//        while (it.hasNext()) {
+//            System.out.println(it.next().getAbsoluteFile());
+//        }
+//    }
+
+    private void print(File obj) {
+        log.info("Got: {}", obj.getAbsoluteFile());
+    }
+
     @Test
-    public void testIteratorExists() {
+    public void testNodeIterableRxFiles() {
         File file = new File("C:\\Users\\rtpk\\Downloads\\New folder");
-        NodeIterable root = new NodeIterable<>(new NodeFile(file));
-        for (Object aRoot : root) {
-            System.out.println(((Node) aRoot).getPayload());
-        }
+        NodeIterable<File> root = new NodeIterable<>(new NodeFile(file));
+        NodeIterableRx<File> temp = new NodeIterableRx<>();
+        BlockingObservable<File> result = temp.convert(root);
+        result.subscribe(this::print);
     }
-
 
     @Test
-    public void testrx() {
+    public void testNodeIterableRxTestObjects() {
         File file = new File("C:\\Users\\rtpk\\Downloads\\New folder");
-        NodeIterable root = new NodeIterable<>(new NodeFile(file));
-        Observable
-                .interval(1, TimeUnit.SECONDS)
-                .from(root::iterator)
-                .distinct()
-                .toBlocking()
-                .subscribe(this::print);
+        NodeIterable<File> root = new NodeIterable<>(new NodeFile(file));
+        NodeIterableRx<File> temp = new NodeIterableRx<>();
+        BlockingObservable<File> result = temp.convert(root);
+        result.subscribe(this::print);
     }
 
-    private void print(Object obj) {
-        log.info("Got: {}", ((Node)obj).getPayload());
-    }
+
+//
+//
+//
+//    List<File> childrenOf(NodeIterable<File> root) {
+//        Iterator<File> it = root.iterator();
+//        List<File> results = (List<File>) StreamSupport.stream(root.spliterator(), false).map(o -> (o)).collect(Collectors.toList());
+//        return results;
+//    }
+//
+//
+//    @Test
+//    public void testrx() {
+//        File file = new File("C:\\Users\\rtpk\\Downloads\\New folder");
+//        NodeIterable<File> root = new NodeIterable<>(new NodeFile(file));
+//        Observable
+//                .interval(1, TimeUnit.SECONDS)
+//                .concatMapIterable(x -> childrenOf(root))
+//                .distinct()
+//                .toBlocking()
+//                .subscribe(this::print);
+//    }
+
 }
